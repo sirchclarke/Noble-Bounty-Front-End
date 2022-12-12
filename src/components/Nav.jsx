586d7c14d4b4d319f37e84ccb62a330b76c1a20c
Chris Clarke-Gonzalez <sirch.clarke@gmail.com>
2022-12-11 23:26:14 -0800

    add nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-const Nav = () => {
-  return (
-    <div>Nav</div>
+const Nav = ({ authenticated, user, handleLogOut }) => {
+
+  const authenticatedOptions = (
+    <nav>
+      <h3>Welcome!</h3>
+      <Link to='/orders'>Orders</Link>
+      <Link to='/addOrder'>Add An Order</Link>
+      <Link to='/customers'>Customers</Link>
+      <Link to= '/search'>Search</Link>
+      <Link to="/">Home</Link>
+      <Link onClick={handleLogOut} to="/">
+        Sign Out
+      </Link>
+      {/* <Link to='/'> <img src={myImage}  alt=""/> </Link> */}
+    </nav>
   )
+
+
+const publicOptions = (
+  <nav>
+    <Link to="/login">Sign In</Link>
+    <Link to="/register">Register</Link>
+    <Link to="/">Home</Link>
+  </nav>
+)
+
+return (
+  <header>
+    <Link to="/">
+    </Link>
+    {authenticated && user ? authenticatedOptions : publicOptions}
+  </header>
+)
 }
 
 export default Nav
