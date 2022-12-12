8d8254c0a06aa7ea449b05c5c121e62799ae107e
Chris Clarke-Gonzalez <sirch.clarke@gmail.com>
2022-12-11 23:08:03 -0800

    orderCard created

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Orders from "../pages/Orders"
 
-const OrderCard = () => {
-  return (
-    <div>OrderCard</div>
-  )
+const OrderCard = (props) => {
+  return(
+    <div className= "order-card" >
+        <div className= "order-info">
+            <h3 className="name-display">Order Name: {props.name}</h3>
+     
+            <button onClick={props.viewOnClick}>View order details</button>
+        </div>
+    </div>
+)
 }
 
 export default OrderCard
