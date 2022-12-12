1ad705265fd2f4aecebe42f8b77b07b0d072e055
Chris Clarke-Gonzalez <sirch.clarke@gmail.com>
2022-12-11 23:22:11 -0800

    added search card

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import useState from 'react'
+import { GetOrder } from "../services/OrderServices"
 
-const Search = () => {
-  return (
-    <div>Search</div>
-  )
+const Search = ({ user, authenticated}) => {
+  const initialState = {
+  customerId: '',
+  orderId: ''
+}
+const [formState, setFormState] = useState(initialState)
+const [ setOrder] = useState(null)
+const [searched, setSearched] = useState(false)
+
+const handleChange = (e) => {
+  setFormState({ ...formState, [e.target.id]: e.target.value })
+}
+
+const onSubmit = async (e) => {
+  e.preventDefault()
+  console.log(formState)
+  const result = await GetOrder(formState)
+  setFormState(initialState)
+  setOrder(result)
+  setSearched(true)
+}
+    <div>
+
+  {authenticated && user ? (
+    
+    
+    <div>
+    <form className="search" onSubmit={onSubmit}>
+
+        <input
+            type="text"
+            id="customerId"
+            value={formState.customerId}
+            placeholder="Customer ID"
+            onChange={handleChange}
+        ></input>
+        <input
+            type="text"
+            id="orderId"
+            value={formState.orderId}
+            placeholder="Order ID"
+            onChange={handleChange}
+        ></input>
+        <button type="submit">Search</button>
+    </form>
+    {searched ? (
+        <div className="search-result">
+        
+        </div>
+    ) : <></>}
+    </div>
+  ) : (
+    <div>
+      <p>You are not signed in</p>
+    </div>
+  )}
+</div>
+
+
+
+
 }
 
 export default Search
